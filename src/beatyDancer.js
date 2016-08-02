925370d884de31f16599335262943a45646ac481
omarmung <dkimmel@gmail.com>
2016-08-01 21:11:19 -0700

    We've Got Blinkers and Beaters

--- src/beatyDancer.js
+++ src/beatyDancer.js
@@ -1,4 +1,4 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
+var makeBeatyDancer = function(top, left, timeBetweenSteps) {
 
   makeDancer.call(this, top, left, timeBetweenSteps);
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
@@ -6,13 +6,13 @@ var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
 
 };
 
-makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
-makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
+makeBeatyDancer.prototype = Object.create(makeDancer.prototype);
+makeBeatyDancer.prototype.constructor = makeBeatyDancer;
 
-makeBlinkyDancer.prototype.step = function() {
+makeBeatyDancer.prototype.step = function() {
   makeDancer.prototype.step.call(this);
 
-  this.$node.toggle();
+  this.$node.slideToggle();
   // toggle() is a jQuery method to show/hide the <span> tag.
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
